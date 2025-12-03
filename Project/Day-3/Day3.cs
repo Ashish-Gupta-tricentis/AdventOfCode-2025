85599c2e69e7c431cb0adc555e9e7ff0138f5de0
Ashish Gupta <ashish.wanted.95@gmail.com>
2025-12-03 23:28:38 +0100

    Added day3

--- Project/Day-3/Day3.cs
+++ Project/Day-3/Day3.cs
@@ -5,8 +5,8 @@ namespace AdventOfCode2025.Day_3;
 
 public class Day3
 {
-    string input = "987654321111111\r\n811111111111119\r\n234234234234278\r\n818181911112111";
-    //string input = File.ReadAllText("Day-3/input.txt");
+    //string input = "987654321111111\r\n811111111111119\r\n234234234234278\r\n818181911112111";
+    string input = File.ReadAllText("Day-3/input.txt");
     public void Part1()
     {
         var lines = input.Split("\r\n");
@@ -32,24 +32,46 @@ public class Day3
         Console.WriteLine(highestPowerInRows.Sum());
     }
 
-    //public void Part2()
-    //{
-    //    var lines = input.Split("\r\n");
-    //    var highestPowerInRows = new List<long>();
-    //    var queueSize = 12;
-    //    foreach (var line in lines)
-    //    {
-    //        int i = 0;
-    //        int j = 12;
-    //        var sb = new StringBuilder();
-    //        for (i = 0; i < line.Length; i++)
-    //        {
+    public void Part2()
+    {
+        var lines = input.Split("\r\n");
+        var size = 12;
+        var highestPowerInRows = new List<long>();
+        foreach (var line in lines)
+        {
+            var result = string.Empty;
+            GetTheResult(line, size, ref result);
+            highestPowerInRows.Add(Convert.ToInt64(result));
+        }
+        Console.WriteLine(highestPowerInRows.Sum());
+    }
 
-    //        }
-    //        highestPowerInRows.Add(Convert.ToInt64(sb.ToString()));
-    //    }
-    //    Console.WriteLine(highestPowerInRows.Sum());
-    //}
+    private void GetTheResult(string line, int size, ref string result)
+    {
+        if (result.Length == size)
+        {
+            return;
+        }
+        var windowSize = line.Length - (size - result.Length - 1);
+        int maxIndex = FindMaxIndex(line.Substring(0, windowSize));
+        result += line[maxIndex];
+        line = line.Remove(0, maxIndex + 1);
+        GetTheResult(line, size, ref result);
+    }
 
+    private int FindMaxIndex(string sub)
+    {
+        char maxChar = sub[0];
+        int maxIndex = 0;
+        for (int i = 1; i < sub.Length; i++)
+        {
+            if (sub[i] > maxChar)
+            {
+                maxChar = sub[i];
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
 }
 
